3f34718284aa524220d286dbcfbc335831386c21
Liz Graham <rileygraham739@gmail.com>
2022-03-03 01:12:52 +0000

    based

--- src/main/java/net/fabricmc/example/ExampleMod.java
+++ src/main/java/net/fabricmc/example/ExampleMod.java
@@ -1,21 +1,47 @@
 package net.fabricmc.example;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
+import net.minecraft.particle.ParticleTypes;
+import net.minecraft.world.World;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Random;
+
 public class ExampleMod implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+		ClientPlayConnectionEvents.JOIN.register((handler, sender, client) -> {
+				ClientTickEvents.START_CLIENT_TICK.register((client1) -> {
 
-		LOGGER.info("Hello Fabric world!");
+					World world = client1.world;
+					if (!client1.player.clientWorld.isSkyVisible(client.player.getBlockPos())) {
+						double d = 0;
+						double e = 0;
+						double f = 0;
+						double probabilityClamped = lerp(64,-64,client.player.getBlockY());
+						for (int i = 0; i < probabilityClamped; i++) {
+							d = client.player.getPos().getX() + getRandomNumberUsingInts(-5, 5);
+							e = client.player.getPos().getY() + getRandomNumberUsingInts(-20, 20);
+							f = client.player.getPos().getZ() + getRandomNumberUsingInts(-5, 5);
+							world.addParticle(ParticleTypes.WHITE_ASH, d, e, f, getRandomNumberUsingInts(-5, 20), getRandomNumberUsingInts(-5, 20), getRandomNumberUsingInts(-5, 20));
+						}
+					}
+				});
+		});
+	}
+
+	public int getRandomNumberUsingInts(int min, int max) {
+		Random random = new Random();
+		return random.ints(min,max)
+				.findFirst()
+				.getAsInt();
+	}
+	public static double lerp(double min, double max, double val) {
+		return 1 - ((val - min) / (max - min));
 	}
 }
